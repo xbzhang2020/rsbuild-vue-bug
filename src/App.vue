612ab3d914eca9ed2c2e5b9f089be66a468f8e4d
zhang <xbzhang2020@icloud.com>
2025-02-01 13:15:22 +0800

    feat: vue bug for hmr

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <div class="content">
+    <h1>Rsbuild with Vue</h1>
+    <p>Start building amazing things with Rsbuild.</p>
+
+    <button @click="count++">+</button>
+    <span>{{ count }}</span>
+    <button @click="count--">-</button>
+  </div>
+</template>
